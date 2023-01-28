b620b5df8edc859a58c59cc57ae7625c117067aa
Arthur Pereira da Silva <arthur.pereira@academico.ifpb.edu.br>
2023-01-28 15:00:21 -0300

    configurando o cors

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,10 +1,19 @@
 import express, { Request, Response, NextFunction } from 'express'
-import { router } from './router';
+import { router } from './router'
+import cors from 'cors'
 import 'express-async-errors'
 
 const app = express();
 
 app.use(express.json())
+app.use(cors())
 app.use(router)
+app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
+    if(err instanceof Error) return res.status(400).json({error: err.message})
+    return res.status(500).json({
+        status: 'error',
+        message: 'Internal server error'
+    })
+})
 
 app.listen(3000, () => console.log("Servidor Online!"))
