e2d6d2b4ffe761fc8bcccfe47b014460413b15bf
Arthur Pereira da Silva <arthur.pereira@academico.ifpb.edu.br>
2023-01-28 15:58:48 -0300

    definindo a arquitetura

--- backend/src/router.ts
+++ backend/src/router.ts
@@ -1,6 +1,6 @@
 import { Request, Response, Router } from "express";
+import { CreateUserController } from "./controllers/user/CreateUserController";
 
 export const router = Router();
-router.get("/teste", (req: Request, res: Response) => {
-    return res.json({ok: true})
-})
+
+router.post('/users', new CreateUserController().handle)
---
+++ backend/src/services/user/CreateUserService.ts
@@ -0,0 +1,14 @@
+interface UserRequest {
+    name: string,
+    email: string,
+    password: string
+}
+
+class CreateUserService {
+    async execute({ name, email, password }: UserRequest) {
+        console.log(name)
+        return {ok: true}
+    }
+}
+
+export { CreateUserService }
